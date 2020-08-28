b7a75b7ac599b6a5238d01ede3bd27b288a0feb5
suf263 <70215660+suf263@users.noreply.github.com>
2020-08-28 18:34:08 +0530

    Add files via upload

---
+++ running python idle.py
@@ -0,0 +1,11 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\user\AppData\Local\Programs\Python\Python37\python idle.py 
+Enter the width:4
+Enter the height:6
+The area is 24 square units
+Enter the base value:6
+Enter the total base value:7
+The area of triangle is 21.0 square units
+>>> 
