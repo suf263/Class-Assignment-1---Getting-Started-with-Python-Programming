8727f4e752689badd4388dffff7f08cb2e3090d9
suf263 <70215660+suf263@users.noreply.github.com>
2020-08-28 18:31:38 +0530

    Add files via upload

---
+++ python idle.py
@@ -0,0 +1,11 @@
+width=int(input("Enter the width:"))
+height=int(input("Enter the height:")) 
+area = width*height
+print("The area is",area,"square units")
+
+base=int(input("Enter the base value:"))
+totalheight=int(input("Enter the total base value:"))
+areaoftriangle = 0.5*base*totalheight
+print("The area of triangle is",areaoftriangle,"square units")
+     
+     
